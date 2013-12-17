26daa2b57e6c212cac98b60e3aa71e5fa3e29472
Jociel <jociel_7@hotmail.com>
2013-12-17 19:48:16 -0300

    Inicio da criação do persistence e genericos

---
+++ SistemaGestaoProjetos/src/main/java/br/com/cast/persistence/dao/GenericoDao.java
@@ -0,0 +1,19 @@
+package br.com.cast.persistence.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface GenericoDao<E extends Serializable> extends JpaDao<E>
+{
+
+   public List<E> listar();
+   
+   public E obter(final Serializable id);
+   
+   public void incluir(final E entidade);
+
+   public void excluir(final E entidade);
+
+   public void alterar(final E entidade);
+
+}
